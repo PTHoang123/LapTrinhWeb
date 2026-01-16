4ee3db4907775f16a1071ae924729848abb7553f
PTHoang123 <hoangphamlol456@gmail.com>
2026-01-16 16:33:48 +0700

    update jakarta

--- ltw-web/src/main/java/controller/ProductDetailServlet.java
+++ ltw-web/src/main/java/controller/ProductDetailServlet.java
@@ -3,10 +3,10 @@ package controller;
 import dao.ProductDAO;
 import model.Product;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ProductDetailServlet extends HttpServlet {
